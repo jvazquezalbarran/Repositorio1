b90bd7b489bee35e8a891131190dd7cc60730e16
jvazquezalbarran <71491527+jvazquezalbarran@users.noreply.github.com>
2025-05-07 16:41:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Tajamar Media</title>
 </head>
 <body>
-<h1 align="center">Buenas dias, estudiantes del Master Tajamar</h1>
+<h1 align="center">Buenas tardes, estudiantes empollones del Master Tajamar</h1>
 <br>
 <div align="center">
 <img src="playa.jpg">
